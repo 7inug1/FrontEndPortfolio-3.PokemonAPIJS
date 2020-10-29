ac35ae0f6ab435cd93c8cc14263a20b56c802628
7inug1 <7inug1@gmail.com>
2020-10-28 22:47:48 -0700

    10/28

---
+++ catAPIjs/script.js
@@ -0,0 +1,38 @@
+const catAPIkey = `c2478ccf-da9c-49cb-9abf-8b41796e6f44`;
+let url = `https://api.thecatapi.com/v1/images/search?`;
+// https://thecatapi.com/v1/images?api_key=c2478ccf-da9c-49cb-9abf-8b41796e6f44
+let result = document.getElementById('result');
+
+getCatByID();
+
+function getCatByID(){
+  fetch(url + new URLSearchParams({
+    breed_id: 'beng'
+    // bar: 2,
+  }), {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'x-api-key': catAPIkey
+    },
+  })
+  .then(response => response.json())
+  .then(data => console.log(data));
+    // result.innerHTML = data;
+}
+
+
+
+
+// function getCatByID(){
+//   fetch(url, { qs: { breed_id: 'beng' } },{
+//     method: 'GET',
+//     headers: {
+//       'Content-Type': 'application/json',
+//       'x-api-key': catAPIkey
+//     },
+//   })
+//   .then(response => response.json())
+//   .then(data => console.log(data));
+//     // result.innerHTML = data;
+// }
