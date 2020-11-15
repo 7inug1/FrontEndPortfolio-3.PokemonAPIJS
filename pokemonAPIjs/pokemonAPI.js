adcc74cba0b7bb51f3ba14ce97e2c65f6ba7f58c
7inug1 <7inug1@gmail.com>
2020-11-15 14:00:14 -0800

    refactor

--- pokemonAPIjs/pokemonAPI.js
+++ pokemonAPIjs/pokemonAPI.js
@@ -1,24 +1,12 @@
-// -------------------------------------------------------------
-// New
-
 // <variables>
 let resultDiv = document.getElementById('resultDiv');
-
-
 let pokemonButton;
 let pokemonArray = [];
-let htmlString = '';
-let pokemonName;
-let pokemonURL;
-let info = [];
 
 // <function calls>
 createPokemonButtons();
-// getPokemonAttributes();
 
 // <functions>
-
-
 function createPokemonButtons(){
   fetch(`https://pokeapi.co/api/v2/pokemon/`)
   .then(response => response.json())
@@ -27,6 +15,7 @@ function createPokemonButtons(){
     // console.log("data.results: " + data.results);
     // console.log("Object.keys(data.results): " + Object.keys(data.results));
     // console.log("data.results.url: " + data.results.url)
+    
     pokemonArray = data.results;
     console.log("pokemonArray: " + pokemonArray);
     
@@ -34,11 +23,9 @@ function createPokemonButtons(){
       pokemonButton = document.createElement("button");
       pokemonButton.textContent = pokemonArray[i].name;
       pokemonButton.addEventListener('click', function() {
-        // getPokemonSprites(pokemonArray[i].url)
         getPokemonAttributes(pokemonArray[i].url)
       }, false);
       let br = document.createElement('br');
-
       resultDiv.appendChild(pokemonButton);
       resultDiv.appendChild(br);
     }
@@ -49,11 +36,6 @@ function getPokemonAttributes(url){
   fetch(url)
   .then(response => response.json())
   .then(data => {
-    console.log("url: " + url);
-    info = data;
-    console.log("info: " + data);
-    console.log("info.sprites: " + data.sprites);
-    // console.log("info.sprites.back_default: " + data.sprites.front_default);
     let img = document.createElement('img');
     img.src = data.sprites.front_default;
     resultDiv.appendChild(img);
